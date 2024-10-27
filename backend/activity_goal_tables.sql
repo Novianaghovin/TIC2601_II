e7d06dbfca46c2ad4d63ee25848711d7ea075b22
0103Yuan <e1062196@u.nus.edu>
2024-10-27 13:56:19 +0800

    Update activity_goal_tables.sql
    insert sample data

--- backend/activity_goal_tables.sql
+++ backend/activity_goal_tables.sql
@@ -35,3 +35,22 @@ CREATE TABLE goals (
         ON DELETE CASCADE 
         ON UPDATE CASCADE
 );
+
+--insert sample data
+INSERT INTO activity_type (activity_name, activity_multiplier) VALUES 
+('Run', 10.00),   -- MET value for running
+('Swim', 8.00),   -- MET value for swimming
+('Cycle', 8.00),  -- MET value for cycling
+('Walk', 3.80);   -- MET value for walking
+
+INSERT INTO activity_log (activity_duration, distance, step_count, calories_burnt, activity_id, user_id) VALUES 
+(30, 5.00, 4000, 300.00, 1, 1),  -- Running for 30 mins
+(60, 1.00, 2000, 350.00, 2, 1),  -- Swimming for 60 mins
+(45, 15.00, 7000, 450.00, 3, 1), -- Cycling for 45 mins
+(30, 2.00, 3000, 150.00, 4, 1);  -- Walking for 30 mins
+
+INSERT INTO goals (goal_name, goal_deadline, progress, user_id, activity_id) VALUES 
+('Run 5 km', '2025-03-31', 0.00, 1, 1),
+('Swim 2 km', '2025-02-28', 0.00, 1, 2),
+('Cycle 50 km', '2025-01-31', 0.00, 1, 3),
+('Walk 10,000 steps', '2025-01-15', 0.00, 1, 4);
