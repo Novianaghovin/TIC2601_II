00442451c7161bde68797821f4ce815a052dddf8
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2024-10-28 13:32:49 +0800

    done

--- backend/database/database.js
+++ backend/database/database.js
@@ -67,8 +67,10 @@ const db = new sqlite3.Database(DB_PATH, (err) => {
             challenge_id INTEGER,
             badge_name TEXT NOT NULL,
             achieved_date DATE DEFAULT (CURRENT_DATE),
-            FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) ON DELETE CASCADE ,
-            FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) ON DELETE CASCADE 
+            FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) 
+            ON DELETE CASCADE ON UPDATE CASCADE ,
+            FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) 
+            ON DELETE CASCADE ON DELETE CASCADE ON UPDATE CASCADE
     )`, (err) => {
         if (err) {
             console.error('Error creating Badge_Records table:', err.message);
@@ -87,8 +89,10 @@ const db = new sqlite3.Database(DB_PATH, (err) => {
             distance DECIMAL(5, 2) NOT NULL,
             time DECIMAL(5, 2) NOT NULL,
             achieved_date DATE DEFAULT (CURRENT_DATE),
-            FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) ON DELETE CASCADE ,
-            FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) ON DELETE CASCADE
+            FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) 
+            ON DELETE CASCADE ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) 
+            ON DELETE CASCADE ON DELETE CASCADE ON UPDATE CASCADE
 
     )`, (err) => {
         if (err) {
