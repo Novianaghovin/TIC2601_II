251fb54f229702c9318ad65317011d79d40be8a8
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2024-10-29 15:10:06 +0800

    Update create_badges&leaderboard.sql
    noticed created double on delete cascade in 1 entity

--- backend/database/create_badges&leaderboard.sql
+++ backend/database/create_badges&leaderboard.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS badge_records (
             FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) 
             ON DELETE CASCADE ON UPDATE CASCADE ,
             FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) 
-            ON DELETE CASCADE ON DELETE CASCADE ON UPDATE CASCADE
+            ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS leaderboard (
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS leaderboard (
             time DECIMAL(5, 2) NOT NULL,
             achieved_date DATE DEFAULT (CURRENT_DATE),
             FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) 
-            ON DELETE CASCADE ON DELETE CASCADE ON UPDATE CASCADE,
+            ON DELETE CASCADE ON UPDATE CASCADE,
             FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) 
-            ON DELETE CASCADE ON DELETE CASCADE ON UPDATE CASCADE
+            ON DELETE CASCADE ON UPDATE CASCADE
 );
