5b26045ea92e314dde608ad0efd8f54808713272
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2024-10-29 21:50:55 +0800

    Update create_badges&leaderboard.sql

--- backend/database/create_badges&leaderboard.sql
+++ backend/database/create_badges&leaderboard.sql
@@ -3,7 +3,7 @@ CREATE TABLE badge_records (
             badge_id INTEGER,
             user_id INTEGER,
             challenge_id INTEGER,
-            badge_name TEXT NOT NULL,
+            badge_name VARCHAR (50) NOT NULL,
             achieved_date DATE DEFAULT (CURRENT_DATE),
             FOREIGN KEY (badge_id) REFERENCES challenge(badge_id) ON DELETE CASCADE ON UPDATE CASCADE,
             FOREIGN KEY (user_id) REFERENCES leaderboard(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -16,7 +16,7 @@ CREATE TABLE leaderboard (
                 challenge_id INTEGER,
                 user_id INTEGER,
                 distance DECIMAL(5, 2) NOT NULL CHECK (distance > 0),
-                time DECIMAL(5, 2) NOT NULL CHECK (distance > 0),
+                time DECIMAL(5, 2) NOT NULL CHECK (time > 0),
                 achieved_date DATE DEFAULT (CURRENT_DATE),
                 FOREIGN KEY (challenge_id) REFERENCES user_challenges(user_challenge_id) ON DELETE CASCADE ON UPDATE CASCADE,
                 FOREIGN KEY (user_id) REFERENCES user_profile(user_id) ON DELETE CASCADE ON UPDATE CASCADE
