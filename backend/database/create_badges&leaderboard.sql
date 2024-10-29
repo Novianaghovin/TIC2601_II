6ef9ec528bf2c0dda94a9b55a1bc7b90e61028aa
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2024-10-29 16:55:58 +0800

    Update create_badges&leaderboard.sql

--- backend/database/create_badges&leaderboard.sql
+++ backend/database/create_badges&leaderboard.sql
@@ -1,26 +1,22 @@
 
-CREATE TABLE IF NOT EXISTS badge_records (
+CREATE TABLE badge_records (
             badge_id INTEGER PRIMARY KEY AUTOINCREMENT,
             user_id INTEGER,
             challenge_id INTEGER,
             badge_name TEXT NOT NULL,
             achieved_date DATE DEFAULT (CURRENT_DATE),
-            FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) 
-            ON DELETE CASCADE ON UPDATE CASCADE ,
-            FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) 
-            ON DELETE CASCADE ON UPDATE CASCADE
+            FOREIGN KEY (user_id) REFERENCES user_profile(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (challenge_id) REFERENCES user_challenges(user_challenge_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS leaderboard (
-            leaderboard_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            rank INTEGER NOT NULL,
-            challenge_id INTEGER,
-            user_id INTEGER,
-            distance DECIMAL(5, 2) NOT NULL,
-            time DECIMAL(5, 2) NOT NULL,
-            achieved_date DATE DEFAULT (CURRENT_DATE),
-            FOREIGN KEY (challenge_id) REFERENCES Challenge(challenge_id) 
-            ON DELETE CASCADE ON UPDATE CASCADE,
-            FOREIGN KEY (user_id) REFERENCES User_Profile(user_id) 
-            ON DELETE CASCADE ON UPDATE CASCADE
+CREATE TABLE leaderboard (
+                leaderboard_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                rank INTEGER NOT NULL CHECK (rank > 0),
+                challenge_id INTEGER,
+                user_id INTEGER,
+                distance DECIMAL(5, 2) NOT NULL CHECK (distance > 0),
+                time DECIMAL(5, 2) NOT NULL CHECK (distance > 0),
+                achieved_date DATE DEFAULT (CURRENT_DATE),
+                FOREIGN KEY (challenge_id) REFERENCES user_challenges(user_challenge_id) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (user_id) REFERENCES user_profile(user_id) ON DELETE CASCADE ON UPDATE CASCADE
 );
