144d2a544a1ca99c74137d2832e310952b1982ba
littleyk <81969367+littleyk@users.noreply.github.com>
2024-11-09 14:33:11 +0800

    Update create_challenges.sql

--- backend/database/create_challenges.sql
+++ backend/database/create_challenges.sql
@@ -25,33 +25,14 @@ CREATE TABLE user_challenges (
     FOREIGN KEY (activity_id) REFERENCES activity_log(log_id)
 );
 
+
 -- Insert data into the avail_challenges table
-INSERT INTO avail_challenges (challenge_type, challenge_deadline, participants_num, status, badge_id) VALUES
-    ('running', DATE('now', '+10 days'), 50, 'Active', 1),
-    ('running', DATE('now', '+5 days'), 30, 'Expired', 2),
-    ('running', DATE('now', '+15 days'), 75, 'Active', 3),
-    ('walking', DATE('now', '+2 days'), 100, 'Expired', 4),
-    ('walking', DATE('now', '+20 days'), 25, 'Active', 5),
-    ('walking', DATE('now', '+1 day'), 60, 'Expired', 6);
+INSERT INTO avail_challenges (challenge_type, challenge_deadline, activity_id, participants_num, status , badge_id, distance) VALUES
+('Running',  '2023-10-31', 1, 50, 'Active', 1, 5),
+('Running',  '2023-11-31', 2, 50, 'Active', 2, 5),
+('Running',  '2023-12-31', 3, 50, 'Active', 3, 5);
 
-
--- sample data
-INSERT INTO user_challenges (user_id, challenge_id, status) VALUES
-(1, 1, 'Active'),
-(2, 2, 'Active'),
-(3, 3, 'Active');
-
-- sample
-INSERT INTO user_challenges (user_id, challenge_id, status) VALUES
-    (1, 1, 'active'),
-    (1, 2, 'completed'),
-    (1, 3, 'expired'),
-    (1, 4, 'completed'),
-    (1, 5, 'active'),
-    (1, 6, 'expired'),
-    (2, 1, 'active'),
-    (2, 2, 'completed'),
-    (2, 3, 'expired'),
-    (2, 4, 'expired'),
-    (2, 5, 'active'),
-    (2, 6, 'completed');
+INSERT INTO user_challenges (user_id, challenge_id,activity_id, status, progress) VALUES
+(1, 1, 1, 'Active', 0.00),
+(2, 2, 2, 'Active', 0.00),
+(3, 3, 3, 'Active', 0.00);
