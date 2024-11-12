9444a1869d3e37b22de3fa7f09b8e8a9db9dfc67
Noviana Ghovin <137738683+Novianaghovin@users.noreply.github.com>
2024-11-12 10:15:09 +0800

    Create app_yuanyuan_donotdelete.js
    yuan yuan app.js coding to revert back

---
+++ backend/router/app_yuanyuan_donotdelete.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+const cors = require('cors'); 
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Import the activity type router
+const activityType = require('./activity_type'); 
+app.use('/activity_type', activityType);
+
+// Import the activity log router
+const activityLog = require('./activity_log');
+app.use('/activity_log', activityLog); // Use the activity log router
+
+
+// Import the goals router
+const goals = require('./goals');
+app.use('/goals', goals); // Use the goals router
+
+// Start the Express server
+app.listen(port, function () {
+    console.log(`Express app listening on port ${port}!`);
+});
