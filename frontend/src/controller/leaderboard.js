267f49533dd939ec22e346f4fb0fee4ad17962ab
littleyk <81969367+littleyk@users.noreply.github.com>
2024-11-16 13:34:46 +0800

    Update leaderboard.js

--- frontend/src/controller/leaderboard.js
+++ frontend/src/controller/leaderboard.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import NavBar from './components/NavBar';
 import './leaderboard.css';
 
 const Leaderboard = () => {
