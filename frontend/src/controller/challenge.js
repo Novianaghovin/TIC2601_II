92d2951b7cc9fe91132279c0e57d05a945f24535
littleyk <81969367+littleyk@users.noreply.github.com>
2024-11-16 13:23:03 +0800

    Update challenge.js

--- frontend/src/controller/challenge.js
+++ frontend/src/controller/challenge.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "./components/NavBar";
 import { useNavigate } from "react-router-dom";
 import useChallenges from "./hooks/useChallenges";
 import ChallengeTable from "./components/challengeTable";
